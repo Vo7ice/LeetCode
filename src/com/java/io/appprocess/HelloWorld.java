af75fd749628ec826e0e230ab53afd4fcd3d0dd4
Vo7ice <Vo7ice@outlook.com>
2018-03-19 20:32:22 +0800

    reversinteger

---
+++ src/com/java/io/appprocess/HelloWorld.java
@@ -0,0 +1,8 @@
+package com.java.io.appprocess;
+
+public class HelloWorld {
+
+    public static void main(String[] args) {
+        System.out.println("Hello, I am started by app_process!");
+    }
+}
---
+++ src/com/java/io/reversinteger/Solution.java
@@ -0,0 +1,24 @@
+package com.java.io.reversinteger;
+
+public class Solution {
+    public int reverse(int x) {
+        System.out.println("max:" + Integer.MAX_VALUE);
+        long ret = 0;
+        while (x != 0) {
+            ret = ret * 10 + x % 10;
+            x = x / 10;
+            if (ret > Integer.MAX_VALUE || ret < Integer.MIN_VALUE) {
+                System.out.println("not available!");
+                return 0;
+            }
+        }
+        return (int) ret;
+    }
+
+    public static void main(String[] args) {
+        Solution sl = new Solution();
+        System.out.println("123 revert:" + sl.reverse(123));
+        System.out.println("-123 revert:" + sl.reverse(-123));
+        System.out.println("120 revert:" + sl.reverse(120));
+    }
+}
