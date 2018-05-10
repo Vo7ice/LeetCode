95596e89b618bc52341cba515334795dc1d4da55
huguojin <Vo7ice@outlook.com>
2018-05-10 14:44:13 +0800

    add length

---
+++ src/com/java/io/length/Solution.java
@@ -0,0 +1,21 @@
+package com.java.io.length;
+
+public class Solution {
+
+    public int lengthOfLastWord(String s) {
+        if (s.trim().isEmpty()) {
+            return 0;
+        }
+        String[] split = s.split(" ");
+        return split[split.length - 1].length();
+    }
+
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+
+        System.out.println("a result:" + solution.lengthOfLastWord("a"));
+        System.out.println("aba result:" + solution.lengthOfLastWord("aba"));
+        System.out.println("' ' result:" + solution.lengthOfLastWord(" "));
+        System.out.println("Hello World result:" + solution.lengthOfLastWord("Hello World"));
+    }
+}
